3797152145a8f204adfabe5c2b9cb361eefb4a77
nallsaa <89607954+nallsaa@users.noreply.github.com>
2022-05-19 18:51:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload(){
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses);
+}
+
+
+function take_snapshot(){
+    save("hi.png")
+}
+
+function draw(){
+    image(video,0,0,300,300)
+    image(mustache, noseX-30, noseY, 75, 30)
+    }
+
+
+function modelLoaded(){
+    console.log("PoseNet is initialized")
+}
+
+noseX = 0
+noseY = 0
+function gotPoses(results){
+    if (results.length > 0){
+
+    console.log(results)
+    noseX = results[0].pose.nose.x
+    noseY = results[0].pose.nose.y
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
+    }
+
+}
+
